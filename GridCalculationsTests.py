ea53b84ab229b37432a9c28c303d295d74d67109
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-05-31 10:58:39 -0600

    A simple combination test

--- GridCalculationsTests.py
+++ GridCalculationsTests.py
@@ -1,5 +1,5 @@
 from ..source.grid.GridCalculations import compute_grid_tree
-from ..source.grid.Grid import Grid
+from ..source.grid.Grid import Grid, RecursivelyDivisibleGridCombination
 from ..source.grid.ReverseCoordinateDoublingGrid import ReverseCoordinateDoublingGrid
 from ..source.GridFactory import SquareRecursiveDivisionGridFactory
 
@@ -13,3 +13,13 @@ class ComputeGridTreeTests(unittest.TestCase):
         self.assertFalse(tree.has_children())
         self.assertEqual(tree.get_value(), simple_grid)
 
+    def test_simple_combination_gives_both_grids(self):
+        primary_simple_grid = SquareRecursiveDivisionGridFactory().create_grid("2")
+        secondary_simple_grid = SquareRecursiveDivisionGridFactory().create_grid("3")
+        combination = RecursivelyDivisibleGridCombination(primary_simple_grid, secondary_simple_grid)
+        tree = compute_grid_tree(combination)
+        self.assertTrue(tree.has_children() and len(tree.get_children()) == 1)
+        self.assertEqual(tree.get_value(), primary_simple_grid)
+        child = tree.get_children()[0]
+        self.assertFalse(child.has_children())
+        self.assertEqual(child.get_value(), secondary_simple_grid)
