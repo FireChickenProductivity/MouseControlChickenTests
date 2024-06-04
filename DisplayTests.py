e556168c09a56d3af7a135c9d9e3b5c4ee40c2a7
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-06-04 16:47:41 -0600

    Test universal position display with alphabetic grid

--- DisplayTests.py
+++ DisplayTests.py
@@ -1,5 +1,5 @@
 from ..source.display.UniversalDisplays import UniversalPositionDisplay
-from ..source.GridFactory import SquareRecursiveDivisionGridFactory, RectangularRecursiveDivisionGridFactory
+from ..source.GridFactory import SquareRecursiveDivisionGridFactory, RectangularRecursiveDivisionGridFactory, AlphabetGridFactory
 from ..source.display.Canvas import Canvas, Text
 from ..source.grid.Grid import Rectangle
 import unittest
@@ -12,6 +12,7 @@ def compute_one_by_two_grid():
 
 def assert_text_element_inside_list(assertion_class: unittest.TestCase, element, elements):
     for e in elements:
+        #print(f"e.x: {e.x}, e.y: {e.y}, e.text: {e.text}")
         if e.x == element.x and e.y == element.y and e.text == element.text:
             return
     assertion_class.fail(f"Element not found in list {element.x}, {element.y}, {element.text}")
@@ -24,9 +25,15 @@ def assert_display_works_for_grid(assertion_class: unittest.TestCase, grid, disp
     display.draw_on(canvas)
     text_manager = canvas.text
     text_elements = text_manager.elements[:]
-    assertion_class.assertEqual(len(expected_text_elements), len(text_elements))
     for element in expected_text_elements:
         assert_text_element_inside_list(assertion_class, element, text_elements)
+    assertion_class.assertEqual(len(expected_text_elements), len(text_elements))
+
+def compute_letter_list():
+    letter_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            ]
+    return letter_list
 
 class UniversalPositionDisplayTest(unittest.TestCase):
     def test_display_with_one_through_four_grid(self):
@@ -49,4 +56,18 @@ class UniversalPositionDisplayTest(unittest.TestCase):
             Text(3, 2, "1"),
             Text(3, 4, "2")
         ]
+        assert_display_works_for_grid(self, grid, display, rectangle, text_elements)
+    
+    def test_display_with_alphabet_grid(self):
+        grid = AlphabetGridFactory().create_grid("")
+        display = UniversalPositionDisplay()
+        start = 0
+        ending = 27
+        rectangle = Rectangle(start, ending, start, ending)
+        text_elements = []
+        letters = compute_letter_list()
+        for i in range(1, ending):
+            for j in range(1, ending):
+                text = letters[j - 1] + " " + letters[i - 1]
+                text_elements.append(Text(i, j, text))
         assert_display_works_for_grid(self, grid, display, rectangle, text_elements)
