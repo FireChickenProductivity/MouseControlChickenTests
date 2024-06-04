18e20d64200743ba19f202c66113c21abb0a04fa
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-06-04 16:08:44 -0600

    Test universal position display with 1by2 grid

--- DisplayTests.py
+++ DisplayTests.py
@@ -1,11 +1,14 @@
 from ..source.display.UniversalDisplays import UniversalPositionDisplay
-from ..source.GridFactory import SquareRecursiveDivisionGridFactory
+from ..source.GridFactory import SquareRecursiveDivisionGridFactory, RectangularRecursiveDivisionGridFactory
 from ..source.display.Canvas import Canvas, Text
 from ..source.grid.Grid import Rectangle
 import unittest
 
 def compute_one_through_four_grid():
     return SquareRecursiveDivisionGridFactory().create_grid("2")
+
+def compute_one_by_two_grid():
+    return RectangularRecursiveDivisionGridFactory().create_grid("1:2")
 
 def assert_text_element_inside_list(assertion_class: unittest.TestCase, element, elements):
     for e in elements:
@@ -31,3 +34,17 @@ class UniversalPositionDisplayTest(unittest.TestCase):
         assert_text_element_inside_list(self, Text(2, 4, "3"), text_elements)
         assert_text_element_inside_list(self, Text(4, 4, "4"), text_elements)
         
+    def test_display_with_one_by_two_grid(self):
+        rectangle = Rectangle(1, 5, 1, 5)
+        grid = compute_one_by_two_grid()
+        grid.make_around(rectangle)
+        display = UniversalPositionDisplay()
+        display.set_rectangle(rectangle)
+        canvas = Canvas()
+        display.set_grid(grid)
+        display.draw_on(canvas)
+        text_manager = canvas.text
+        text_elements = text_manager.elements[:]
+        self.assertEqual(2, len(text_elements))
+        assert_text_element_inside_list(self, Text(3, 2, "1"), text_elements)
+        assert_text_element_inside_list(self, Text(3, 4, "2"), text_elements)
