64e85c99d8ebe404b4e40d0c0821a6c079bfce2f
Fire Chicken <firechickenproductivity@gmail.com>
2025-02-14 17:19:52 -0700

    Test option based argument types

--- GridFactoryArgumentTypesTests.py
+++ GridFactoryArgumentTypesTests.py
@@ -1,6 +1,9 @@
-from ..source.grid_creation.GridFactoryArgumentTypes import TwoToNineArgumentType, PositiveIntegerArgumentType, GridOptionArgumentType, CustomCoordinateSystemArgumentType
+from ..source.grid_creation.GridFactoryArgumentTypes import TwoToNineArgumentType, PositiveIntegerArgumentType, GridOptionArgumentType, CustomCoordinateSystemArgumentType, GRID_CREATION_ARGUMENT_TWO_TO_NINE_TAG, GRID_CREATION_ARGUMENT_POSITIVE_INTEGER_TAG, ARGUMENT_INPUT_THROUGH_DICTATION_INPUT_TAG
 import unittest
 from .TestUtilities import assert_true_for_all, assert_false_for_all
+
+DEFAULT_GRID_OPTION_NAMES = ["one to nine", "alphabet", "double alphabet", "alphabet numbers", "double alphabet numbers"]
+EXPECTED_CUSTOM_COORDINATE_SYSTEM_FILE_NAME = "newer"
 
 class GridFactoryArgumentTypesTests(unittest.TestCase):
     def test_two_to_nine_argument_type_with_valid_values(self):
@@ -17,6 +20,10 @@ class GridFactoryArgumentTypesTests(unittest.TestCase):
         argument_type = TwoToNineArgumentType()
         self.assertFalse(argument_type.supports_options_dialogue())
 
+    def test_to_two_nine_argument_type_supports_correct_tags(self):
+        argument_type = TwoToNineArgumentType()
+        self.assertEqual([ARGUMENT_INPUT_THROUGH_DICTATION_INPUT_TAG, GRID_CREATION_ARGUMENT_TWO_TO_NINE_TAG], argument_type.get_tags())
+
     
     def test_positive_integer_argument_type_with_valid_values(self):
         argument_type = PositiveIntegerArgumentType()
@@ -32,3 +39,55 @@ class GridFactoryArgumentTypesTests(unittest.TestCase):
         argument_type = PositiveIntegerArgumentType()
         self.assertFalse(argument_type.supports_options_dialogue())
     
+    def test_positive_integer_argument_type_supports_correct_tags(self):
+        argument_type = PositiveIntegerArgumentType()
+        self.assertEqual([ARGUMENT_INPUT_THROUGH_DICTATION_INPUT_TAG, GRID_CREATION_ARGUMENT_POSITIVE_INTEGER_TAG], argument_type.get_tags())
+
+    
+    def test_grid_option_argument_type_with_valid_values(self):
+        argument_type = GridOptionArgumentType()
+        valid_values = DEFAULT_GRID_OPTION_NAMES
+        assert_true_for_all(self, argument_type.does_argument_match_type, valid_values)
+
+    def test_grid_option_argument_type_with_invalid_values(self):
+        argument_type = GridOptionArgumentType()
+        invalid_value= "no99"
+        self.assertFalse(argument_type.does_argument_match_type(invalid_value))
+
+    def test_grid_option_argument_type_supports_options_dialogue(self):
+        argument_type = GridOptionArgumentType()
+        self.assertTrue(argument_type.supports_options_dialogue())
+
+    def test_grid_option_argument_type_supports_correct_tags(self):
+        argument_type = GridOptionArgumentType()
+        self.assertEqual([], argument_type.get_tags())
+
+    def test_grid_option_argument_type_has_expected_options(self):
+        argument_type = GridOptionArgumentType()
+        actual_options = argument_type.get_options()
+        for option in DEFAULT_GRID_OPTION_NAMES:
+            self.assertTrue(option in actual_options)
+
+    
+    def test_custom_coordinate_system_argument_type_with_valid_value(self):
+        argument_type = CustomCoordinateSystemArgumentType()
+        valid_value = EXPECTED_CUSTOM_COORDINATE_SYSTEM_FILE_NAME
+        self.assertTrue(argument_type.does_argument_match_type(valid_value))
+
+    def test_custom_coordinate_system_argument_type_with_invalid_value(self):
+        argument_type = CustomCoordinateSystemArgumentType()
+        invalid_value = "undefined.csv"
+        self.assertFalse(argument_type.does_argument_match_type(invalid_value))
+
+    def test_custom_coordinate_system_argument_type_supports_options_dialogue(self):
+        argument_type = CustomCoordinateSystemArgumentType()
+        self.assertTrue(argument_type.supports_options_dialogue())
+
+    def test_custom_coordinate_system_argument_type_supports_correct_tags(self):
+        argument_type = CustomCoordinateSystemArgumentType()
+        self.assertEqual([], argument_type.get_tags())
+
+    def test_custom_coordinate_system_argument_type_has_expected_options(self):
+        argument_type = CustomCoordinateSystemArgumentType()
+        actual_options = argument_type.get_options()
+        self.assertTrue(EXPECTED_CUSTOM_COORDINATE_SYSTEM_FILE_NAME in actual_options)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MouseControlChickenTests
 Testing code for MouseControlChicken.
+
+## Setup Assumptions
+This code is supposed to be placed inside the MouseControlChicken directory.
+
+Some of the test code assumes that the 5 default grid options are available and the user does not have a grid option called "no99".
+
+The user is assumed to have a custom coordinate system called newer.csv and to not have a custom coordinate system called undefined.csv.
